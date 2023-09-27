6bb096c87750045769e6def0f424908cc1efa437
Manuel Zambrano <89891295+manuelz8@users.noreply.github.com>
2023-09-27 19:18:40 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import instagram as ig
 
 app = Flask(__name__)
-app.config['TIMEOUT'] = 50
+app.config['TIMEOUT'] = 0
 
 @app.route('/')
 def instagram_media():
@@ -14,19 +14,40 @@ def get_instagram_content():
         data = request.get_json()
 
         url = data.get("url")
-        content_data = ig.get_instagram_media(url)
+        multi_url = data.get("multi_url")
 
-        if content_data['success'] == False:
-            return content_data, 400
+        if url is None and multi_url is None:
+            return {"error": "URL is missing"}, 400
 
-        audit_data = {
-            'username': content_data['username'],
-            'media_url': content_data['media_url'],
-            'followers': content_data['followers'],
-        }
-        ig.instagram_audit(audit_data)
+        if type(multi_url) == list:
+            response = []
+            for url in multi_url:
+                content_data = ig.get_instagram_media(url)
 
-        return content_data, 200
+                audit_data = {
+                    'username': content_data['username'],
+                    'media_url': content_data['media_url'],
+                    'followers': content_data['followers'],
+                }
+
+                ig.instagram_audit(audit_data)
+
+                response.append(content_data)
+            return { 'data': tuple(response) }, 200
+        else:
+            content_data = ig.get_instagram_media(url)
+
+            if content_data['success'] == False:
+                return {'data': content_data}, 400
+
+            audit_data = {
+                'username': content_data['username'],
+                'media_url': content_data['media_url'],
+                'followers': content_data['followers'],
+            }
+            ig.instagram_audit(audit_data)
+
+            return content_data, 200
 
     except Exception as e:
         return {"error": str(e)}, 500
